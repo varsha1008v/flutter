6ef12c8752e1f81bd8a38206477c3ae3ead6ce6c
varsha1008v <varshasatish1008@gmail.com>
2025-05-31 12:10:46 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\varsh\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\varsh\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\varsh\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YjI1MzA1YTg4Mw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTQyNWU1ZTllYw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\varsh\\OneDrive\\Desktop\\flutter\\flutter_application_1\\.dart_tool\\package_config.json"
+)
